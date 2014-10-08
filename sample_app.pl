956e629b319187f5c9cfcfc81f010c1680b7e83b
reneeb <github@renee-baecker.de>
2014-10-09 00:22:48 +0200

    add wrk results

--- sample_app.pl
+++ sample_app.pl
@@ -92,3 +92,27 @@ get non_blocking => sub {
 };
 
 app->start;
+
+__END__
+$ ./wrk -t8 -d30s -c 10 http://127.0.0.1:3000/blocking
+Running 30s test @ http://127.0.0.1:3000/blocking
+  8 threads and 10 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     1.44s     1.66s    2.88s   100.00%
+    Req/Sec     0.00      0.00     0.00    100.00%
+  10 requests in 30.02s, 13.29KB read
+  Socket errors: connect 0, read 8, write 0, timeout 101
+Requests/sec:      0.33
+Transfer/sec:     453.29B
+
+
+$ ./wrk -t8 -d30s -c 10 http://127.0.0.1:3000/non_blocking
+Running 30s test @ http://127.0.0.1:3000/non_blocking
+  8 threads and 10 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     3.47s     1.14s    5.58s    65.38%
+    Req/Sec     0.00      0.00     0.00    100.00%
+  79 requests in 30.02s, 92.91KB read
+  Socket errors: connect 0, read 0, write 0, timeout 42
+Requests/sec:      2.63
+Transfer/sec:      3.10KB
